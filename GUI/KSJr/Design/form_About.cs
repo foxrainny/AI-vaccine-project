542d9b067fb738f2e3e5cd45486c674601646fde
RubyTale <ghdhfk901@gmail.com>
2020-06-23 14:14:15 +0900

    Add files via upload

---
+++ GUI/KSJr/Design/form_About.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XanderUI;
+
+namespace KSJr
+{
+    public partial class form_About : Form
+    {
+        public form_About()
+        {
+            InitializeComponent();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ GUI/KSJr/Design/form_Main.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KSJr
+{
+    public partial class form_Main : Form
+    {
+        public form_Main()
+        {
+            form_About about_Form = new form_About();
+            about_Form.ShowDialog();
+
+            InitializeComponent();
+        }
+        private void button_result_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Search_Click(object sender, EventArgs e)
+        {
+            list_View.Items.Clear();
+
+            String dir_path = null;
+
+            
+            
+
+            
+            
+
+
+            if (folderBD.ShowDialog() == DialogResult.OK)
+            {
+
+                DateTime localDate = DateTime.Now;
+                now_label.Text = "Scan Time : " + localDate.ToString(); 
+                try
+                {
+                    dir_path = folderBD.SelectedPath;
+
+
+                    //testBox.Text = folderBD.SelectedPath;
+                }
+                catch(Exception exp)
+                {
+                    MessageBox.Show("Error"
+                        + Environment.NewLine + exp.ToString() + Environment.NewLine);
+                    this.Dispose();
+                }
+                Invalidate();
+
+
+            }
+            else
+            {
+                return;
+            }
+
+
+            list_View.BeginUpdate();
+
+
+            list_View.Columns.Add("파일명", 999, HorizontalAlignment.Left);
+
+            //new Preloader().Show();
+
+            dirSearch(dir_path);
+
+            python_test();
+
+
+            //list_View.EnsureVisible(list_View.Items.Count - 1);
+            list_View.EndUpdate();
+
+            //
+        }
+
+        private void dirSearch(string dir)
+        {
+            string[] Dirs = Directory.GetDirectories(dir);
+            {
+                string[] Files = Directory.GetFiles(dir);
+                
+
+                foreach (var fileName in Files)
+                {
+                    
+
+                    bool b = WinTrust.VerifyEmbeddedSignature(fileName);
+                    
+
+                    String str = b.ToString();
+                    textBox_test.Text = str;
+
+
+                    
+                    list_View.Items.Add(fileName);
+                }
+                foreach (var nodeDir in Dirs)
+                {
+                    list_View.Items.Add(nodeDir);
+                    dirSearch(nodeDir);
+                }
+            }
+        }
+
+
+
+        private void python_test()
+        {
+            var engine = IronPython.Hosting.Python.CreateEngine();
+            var scope = engine.CreateScope();
+
+            try
+            {
+                var source = engine.CreateScriptSourceFromFile(@"test.py");
+                source.Execute(scope);
+
+                var getPythonFuncResult = scope.GetVariable<Func<string>>("getPythonFunc");
+                Console.WriteLine("def 실행 테스트 : " + getPythonFuncResult());
+
+                var sum = scope.GetVariable<Func<int, int, int>>("sum");
+                Console.WriteLine(sum(1, 2));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
